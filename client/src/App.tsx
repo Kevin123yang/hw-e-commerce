ca8593971b72c1fcafb3c18ccb8af17906dd4fe6
Kevin123yang <wenkai.yang00@gmail.com>
2026-05-20 22:25:04 -0400

    day18
    setup query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
 import { MantineProvider } from "@mantine/core";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
-
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 import "@mantine/core/styles.css";
 
 function App() {
   return (
-    <MantineProvider>
-      <RouterProvider router={router} />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <RouterProvider router={router} />
+      </MantineProvider>
+    </QueryClientProvider>
   );
 }
 
---
+++ client/src/lib/axiosClient.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const axiosClient = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+axiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
