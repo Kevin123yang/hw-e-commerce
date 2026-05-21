260cf3a07fa82800dd2bbfa63a3c96cd0eb3eecc
Kevin123yang <wenkai.yang00@gmail.com>
2026-05-21 02:15:53 -0400

    day 18
    fix type error

--- client/src/features/products/hooks/useProduct.ts
+++ client/src/features/products/hooks/useProduct.ts
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchProduct } from "../api/productsApi";
+import type { Product } from "../type/products";
 
 export function useProduct(id:number) {
-  return useQuery({
+  return useQuery<Product>({
     queryKey: ["product", id],
     queryFn: ()=> fetchProduct(id),
   });
--- client/src/features/products/hooks/useProducts.ts
+++ client/src/features/products/hooks/useProducts.ts
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchProducts } from "../api/productsApi";
 import type { ProductsFilers } from "../type/ProductsFilers";
+import type { ProductsResponse } from "../type/products";
 
 
 export function useProducts(filers?: ProductsFilers) {
-  return useQuery({
+  return useQuery<ProductsResponse>({
     queryKey: ["products", filers],
     queryFn: ()=> fetchProducts(filers),
   });
